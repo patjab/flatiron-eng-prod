ce8ffc455fa3fd7bf46304eaffb5e4af719db78e
github username <github email address>
2018-09-27 04:13:34 -0400

    successfully fetched user data from github API

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,11 +2,40 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 
 class Results extends Component {
+  state = {
+    currentUser: null
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return !(nextProps.username === this.props.username)
+  // }
+
+  componentDidUpdate() {
+    fetch(`https://api.github.com/users/${this.props.username}`)
+    .then(r => r.json())
+    .then(data => {
+      if ( data.message === "Not Found"  ) {
+        this.setState({currentUser: null})
+      } else {
+        console.log(data)
+        this.setState({currentUser: data}, () => console.log(this.state.currentUser.avatar_url))
+      }
+    })
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        { this.state.currentUser ? <img src={this.state.currentUser.avatar_url} /> : null }
+      </div>
     )
   }
 }
 
-export default Results
+const mapStateToProps = (state) => {
+  return {
+    username: state.username
+  }
+}
+
+export default connect(mapStateToProps)(Results)
