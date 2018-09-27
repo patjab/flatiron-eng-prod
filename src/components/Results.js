6bc608095d860d959ca153bb08d8b6dfe6782b10
github username <github email address>
2018-09-27 03:42:16 -0400

    created Results component for display

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+
+class Results extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Results
--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 
 import UsernameInput from './UsernameInput'
+import Results from './Results'
 
 class SearchContainer extends Component {
   render() {
-    return <UsernameInput />
+    return (
+      <Fragment>
+        <UsernameInput />
+        <Results />
+      </Fragment>
+    )
   }
 }
 
